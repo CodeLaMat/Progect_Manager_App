740ad3ec1f936dd2344e63883c34fb0ece40e081
CodeLaMat <eyvaz.alishov@edu.bc.fi>
2023-02-08 12:20:23 +0200

    namespace syntax applied

--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,15 @@
-function autobind(
-  target: any,
-  methodName: String,
-  destructor: PropertyDescriptor
-);
-{
-  const originalMethod = descriptor.value;
-  const adjDescriptor = (PropertyDescriptor = {
-    configurable: true,
-    get() {
-      const boundFn = originalMethod.bind(this);
-      return boundFn;
-    },
-  });
-  return adjDescriptor;
+/// <reference path="./models/drag-and-drop.ts" />
+/// <reference path="./models/project-model.ts" />
+/// <reference path="./state/project-state.ts" />
+/// <reference path="./uti/validator.ts" />
+/// <reference path="./decorators/autobind.ts" />
+/// <reference path="./components/base-component.ts" />
+/// <reference path="./components/project-input.ts" />
+/// <reference path="./components/project-item.ts" />
+/// <reference path="./components/project-list.ts" />
+
+namespace App {
+  new ProjectInput();
+  new ProjectList("active");
+  new ProjectList("finished");
 }
-
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
-
-  titleInputElement: HTMLInputElement;
-  descriptionInputElement: HTMLInputElement;
-  peopleInputElement: HTMLInputElement;
-
-  constructor() {
-    this.templateElement = document.getElementById(
-      "project-input"
-    )! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
-
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
-
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = "user-input";
-    this.titleInputElement = this.element.querySelector(
-      "#title"
-    ) as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector(
-      "#description"
-    ) as HTMLInputElement;
-    this.peopleInputElement = this.element.querySelector(
-      "#people"
-    ) as HTMLInputElement;
-
-    this.attach();
-    this.configure();
-  }
-  @autobind
-  private submitHandler(event: Event) {
-    event.preventDefault();
-    console.log(this.titleInputElement.value);
-  }
-
-  private configure() {
-    this.element.addEventListener("submit", this.submitHandler);
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
-  }
-}
-
-const prjInput = new ProjectInput();
